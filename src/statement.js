c3ee17a4596678fc2562a3a97403e4d29f723fce
Chongfa <cfachen@thoughtworks.com>
2019-01-24 19:47:59 +0800

    afaren - refactor, replace temp with query, playFor

--- src/statement.js
+++ src/statement.js
@@ -7,7 +7,7 @@ function statement (invoice, plays) {
                           minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // add volume credits
@@ -45,6 +45,11 @@ function statement (invoice, plays) {
     }
     return result;
   }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
 }
 
 module.exports = statement;
