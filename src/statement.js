49b5ecb45c2ee18b41d72022c778f57ffcea2c9e
Chongfa <cfachen@thoughtworks.com>
2019-01-24 20:06:17 +0800

    afaren - refactor, split loop & slide statements

--- src/statement.js
+++ src/statement.js
@@ -1,18 +1,21 @@
 function statement (invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
 
     for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += volumeCreditsFor(perf);
-
     //print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount/100)}\n`;
+
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    // add volume credits
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
+    result += `Amount owed is ${usd(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
